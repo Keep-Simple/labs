c6d1dbff0cc8246cc1490b76d0cc788df16052e5
nick <nikyasnogorodskyi@gmail.com>
2021-09-03 12:44:20 +0300

    feat: 0lab + folder structure

---
+++ 1_semester/0/0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* #include <conio.h> */
+
+int main() {
+	char name[15];
+	printf("Enter your name: ");
+	scanf("%s", name);
+	printf("Hello \"\"%s\"\"! You are welcome to C world\r\n", name);
+}
+
