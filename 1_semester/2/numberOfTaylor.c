91162f27a283d8202609b64c707722a8878dd8c6
nick <nikyasnogorodskyi@gmail.com>
2021-09-03 15:12:57 +0300

    feat: numberOfTaylor implementation

---
+++ 1_semester/2/numberOfTaylor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+double _numberOfTaylor(double x, double precision, int step, double sum, double prevElement)
+{
+    double currentElement = (prevElement * x) / ++step;
+    if (precision >= fabs(currentElement))
+    {
+        return sum;
+    }
+    return _numberOfTaylor(x, precision, step, sum + currentElement, currentElement);
+}
+
+double numberOfTaylor(double x, double precision)
+{
+    return _numberOfTaylor(x, precision, 0, 0, 1);
+}
+
+int main()
+{
+    double x, eps;
+
+    printf("Enter x: ");
+    scanf("%lf", &x);
+    printf("Enter eps: ");
+    scanf("%lf", &eps);
+
+    double result = numberOfTaylor(x, eps);
+
+    printf("Result = %lf", result);
+
+    return 0;
+}
