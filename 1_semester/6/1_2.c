76fc781bea4aa100bb8ba77cdadbc9baf08ba826
nick <nikyasnogorodskyi@gmail.com>
2021-09-09 09:13:04 +0300

    feat: complete 6.2

--- 1_semester/6/1_2.c
+++ 1_semester/6/1_2.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-// void calcArgsDigits(int firstNumber, ...)
-// {
-//     int currentNum = firstNumber;
-//     va_list args;
-//     va_start(args, &firstNumber);
+void parseNumber(int);
 
-//     // read args while current element not -1
-//     while ((currentNum = va_arg(args, int)) != -1)
-//     {
-//         printf("Number: %d", currentNum);
-//     }
-//     va_end(args);
-// }
 void calcArgsDigits(int firstNumber, ...)
 {
-    int *argsCursor = &firstNumber;
+    int currentNum;
+    va_list args;
+    va_start(args, &firstNumber);
 
+    parseNumber(firstNumber);
     // read args while current element not -1
-    while (*argsCursor != -1)
+    while ((currentNum = va_arg(args, int)) != -1)
     {
-        printf("Number: %d\n", *argsCursor);
-        argsCursor++;
+        parseNumber(currentNum);
+    }
+    va_end(args);
+}
+
+typedef struct DigitFrequency
+{
+    int digit;
+    int count;
+} digitFrequency;
+
+void parseNumber(int number)
+{
+    int numberCopy = number;
+    digitFrequency map[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        map[i].digit = i;
+        map[i].count = 0;
+    }
+
+    while (numberCopy)
+    {
+        int digit = numberCopy % 10;
+        map[digit].count++;
+        numberCopy /= 10;
+    }
+
+    printf("Parsing number \"%d\":\n", number);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\t\"%d\" used %d times\n", map[i].digit, map[i].count);
     }
 }
 
@@ -30,7 +53,7 @@ int main()
 {
     printf("Task 5, Section 2\n");
 
-    calcArgsDigits(1, 2, 3, 4, 5, 6, -1);
+    calcArgsDigits(1000, 22394, 3393939, 4999, 51111, 6120390756, -1);
 
     return 0;
 }
