df5fbf42c0777efda945deb11373ab24f14d7cbf
Nikita Yasnogorodskyi <nikita@orca.security>
2022-04-30 21:06:14 +0300

    feat: na 4th lab

--- 2_term/numerical_analysis(ЧМ)/l4/l4.py
+++ 2_term/numerical_analysis(ЧМ)/l4/l4.py
@@ -13,34 +13,102 @@ A = np.array(
 B = np.array([2.71, 5.26, 1.03])
 
 
-def gaussElim(a, b):
-    n = len(b)
+def gauss_elim(A, B):
+    n = len(B)
 
     # Elimination phase
     for k in range(n - 1):
         # find the best raw
         for i in range(k + 1, n):
-            if fabs(a[i, k]) > fabs(a[k, k]):
-                a[[k, i]] = a[[i, k]]
-                b[[k, i]] = b[[i, k]]
+            if fabs(A[i, k]) > fabs(A[k, k]):
+                A[[k, i]] = A[[i, k]]
+                B[[k, i]] = B[[i, k]]
                 break
 
         for i in range(k + 1, n):
-            if a[i, k] != 0.0:
-                cof = a[i, k] / a[k, k]
+            if A[i, k] != 0.0:
+                cof = A[i, k] / A[k, k]
                 # calculate the new row
-                a[i] = a[i] - cof * a[k]
+                A[i] = A[i] - cof * A[k]
                 # update vector b
-                b[i] = b[i] - cof * b[k]
-        print(f"Iteration {k}:")
-        _print_a_and_b(a, b)
+                B[i] = B[i] - cof * B[k]
+        print(f"\nIteration {k}:")
+        _print_a_and_b(A, B)
 
     # backward substitution
     x = np.zeros(n)
     for k in range(n - 1, -1, -1):
-        x[k] = (b[k] - np.dot(a[k, k + 1 : n], x[k + 1 : n])) / a[k, k]
+        x[k] = (B[k] - np.dot(A[k, k + 1 : n], x[k + 1 : n])) / A[k, k]
 
     return x
+
+
+def lu_solve(L, U, P, B):
+    def forward_sub(L, b):
+        """solution to Lx = b
+        L must be a lower-triangular matrix
+        b must be a vector of the same leading dimension as L
+        """
+        n = len(L)
+        x = np.zeros(n)
+        for i in range(n):
+            sum = 0
+            for j in range(i):
+                sum += L[i, j] * x[j]
+            x[i] = (b[i] - sum) / L[i, i]
+        return x
+
+    def back_sub(U, b):
+        """solution to Ux = b
+        U must be an upper-triangular matrix
+        b must be a vector of the same leading dimension as U
+        """
+        n = len(U)
+        x = np.zeros(n)
+        for i in range(n - 1, -1, -1):
+            sum = 0
+            for j in range(n - 1, i, -1):
+                sum += U[i, j] * x[j]
+            x[i] = (b[i] - sum) / U[i, i]
+        return x
+
+    y = forward_sub(L, np.dot(P, B))
+    x = back_sub(U, y)
+    return x
+
+
+def lu_decompose(A):
+    """
+    Decomposes a matrix A by PA=LU
+    """
+
+    def pivotize(m):
+        """
+        Creates the pivoting matrix for m.
+        """
+        n = len(m)
+        ID = [[float(i == j) for i in range(n)] for j in range(n)]
+        for j in range(n):
+            row = max(range(j, n), key=lambda i: abs(m[i][j]))
+            if j != row:
+                ID[j], ID[row] = ID[row], ID[j]
+        return ID
+
+    n = len(A)
+    L = np.zeros((n, n))
+    U = np.zeros((n, n))
+    P = np.asarray(pivotize(A))
+    PA = np.dot(P, A)
+    for j in range(n):
+        L[j][j] = 1.0
+        for i in range(j + 1):
+            s1 = sum(U[k][j] * L[i][k] for k in range(i))
+            U[i][j] = PA[i][j] - s1
+        for i in range(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in range(j))
+            L[i][j] = (PA[i][j] - s2) / U[j][j]
+
+    return L, U, P
 
 
 def _print_a_and_b(a, b):
@@ -48,7 +116,6 @@ def _print_a_and_b(a, b):
     print_matrix(a)
     print("B:")
     print(b)
-    print()
 
 
 def print_matrix(mtrx):
@@ -63,12 +130,25 @@ if __name__ == "__main__":
     print("Initial values:")
     _print_a_and_b(A, B)
 
-    A_COPY = A.copy()  # save original matrix A
-    B_COPY = B.copy()  # save original vector b
-    X = gaussElim(A, B)
+    print("\n\nGause method:")
+    x = gauss_elim(A.copy(), B.copy())
+    print("\nResulting vector x:")
+    print(x)
+    print("\nVerifying results: AX - B = ", np.dot(A, x) - B)
 
-    print("\nResults, X:")
-    print(X)
+    print("\n\nLU method:")
+    L, U, P = lu_decompose(A.copy())
 
-    print("\nChecking results: AX - B = ", np.dot(A_COPY, X) - B_COPY)
-    print()
+    print("\nL:")
+    print_matrix(L)
+    print("\nU:")
+    print_matrix(U)
+    print("\nP:")
+    print_matrix(P)
+    print("\nIs LU = PA:", np.array_equal(np.dot(L, U), np.dot(P, A)))
+
+    x = lu_solve(L, U, P, B.copy())
+    print("\nResulting vector x:")
+    print(x)
+
+    print("\nVerifying results: AX - B = ", np.dot(A, x) - B)
