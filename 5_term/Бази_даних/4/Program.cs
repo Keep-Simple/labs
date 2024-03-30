243c29df1f993c51487382aa7eaacc5fd3b12150
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2024-03-30 19:49:20 +0100

    feat: Бази_даних 5/6

---
+++ 5_term/Бази_даних/4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=suppliers.db";
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            CreateSuppliersTable(connection);
+            InsertSupplier(connection, "Company A", "Address 1");
+            InsertSupplier(connection, "Company B");
+
+            DisplaySuppliers(connection);
+        }
+    }
+
+    static void CreateSuppliersTable(SqliteConnection connection)
+    {
+        string createTableQ = @"
+            CREATE TABLE IF NOT EXISTS suppliers (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                address TEXT DEFAULT 'Not provided'
+            );";
+
+        ExecuteNonQuery(connection, createTableQ);
+    }
+
+    static void InsertSupplier(SqliteConnection connection, string name, string address = "Not provided")
+    {
+        string insertQ = "INSERT INTO suppliers (name, address) VALUES (@name, @address);";
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = insertQ;
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@address", address);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    static void DisplaySuppliers(SqliteConnection connection)
+    {
+        string selectAllQ = "SELECT * FROM suppliers;";
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = selectAllQ;
+            using (var reader = command.ExecuteReader())
+            {
+                Console.WriteLine("id\tname\taddress");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["id"]}\t{reader["name"]}\t{reader["address"]}");
+                }
+            }
+        }
+    }
+
+    static void ExecuteNonQuery(SqliteConnection connection, string query)
+    {
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ 5_term/Бази_даних/5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (!IsValidArguments(args))
+        {
+            Console.WriteLine("Потрібно вказати шлях до файлу бази даних (-db) та назву таблиці (-t).");
+            return;
+        }
+
+        string dbFile = args[1];
+        string tableName = args[3];
+        bool verbose = args.Length >= 5 && args[4] == "-V";
+
+        using (var connection = new SqliteConnection($"Data Source={dbFile}"))
+        {
+            connection.Open();
+
+            string selectQuery = $"SELECT * FROM {tableName};";
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = selectQuery;
+                using (var reader = command.ExecuteReader())
+                {
+                    if (verbose)
+                    {
+                        PrintFieldDetails(reader);
+                    }
+
+                    PrintData(reader);
+                }
+            }
+        }
+    }
+
+    static bool IsValidArguments(string[] args)
+    {
+        return args.Length >= 4 && args[0] == "-db" && args[2] == "-t";
+    }
+
+    static void PrintFieldDetails(SqliteDataReader reader)
+    {
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+            Console.WriteLine($"Поле {reader.GetName(i)}: тип {reader.GetFieldType(i)}");
+        }
+    }
+
+    static void PrintData(SqliteDataReader reader)
+    {
+        while (reader.Read())
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write(reader[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
